daf37bf41c54741f5fbaa841985613a1d2aea543
kylefang19 <kylefang19@g.ucla.edu>
2020-03-02 11:16:55 -0800

    new sorting function

--- sort_max_time_fxn.m
+++ sort_max_time_fxn.m
@@ -1,16 +1,11 @@
 % Written by SAW, 012220
 % Function that sorts one matrix by the time of maximum signal, then sorts
 % a second matrix in the exact same order. 
-function [sortedmat, sortedmat2] = sort_max_time(IS, SI)
+function [sortedmat, sortedmat2, sortedmat3, sortedmat4] = sort_max_time(IS, SI)
 
 % Define the transition matrix to look at
  trans_sig = IS;
  other_sig = SI;
- 
-% new code below
-%  trans_sig = SI;
-%  other_sig = IS;
-
 
  % Sort that matrix by time of maximum value
  [M,I] = max(trans_sig');
@@ -23,6 +18,20 @@ function [sortedmat, sortedmat2] = sort_max_time(IS, SI)
  new2 = [special other_sig];
  sortedmat2 = sortrows(new2,1);
  sortedmat2(:,1) = [];
+ 
+ % Do the reverse now
+ [Q,S] = max(other_sig');
+ unique = (S');
+ new3= [unique other_sig];
+ sortedmat3 = sortrows(new3,1);
+ sortedmat3(:,1) = [];
+ 
+ new4 = [unique trans_sig];
+ sortedmat4 = sortrows(new4,1);
+ sortedmat4(:,1) = [];
+ 
+ 
+ 
  
  % Plot sorted figure
  %figure
